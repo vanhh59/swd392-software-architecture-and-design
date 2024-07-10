51548cdf1bddaa0020992574d83973c7fc6aabbd
quangnguyen3007 <nguyenducquang3007@gmail.com>
2024-07-10 23:46:45 +0700

    week9

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://babyboo-backend-test.onrender.com",
-      "/uploads/": "https://babyboo-backend-test.onrender.com",
+      "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000",
     },
   },
 });
